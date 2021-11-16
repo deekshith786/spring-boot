1725462bba8acbeadd5b58d76914d6a5af641668
Deekshith <76718957+deekshith786@users.noreply.github.com>
2021-11-16 14:46:03 +0530

    Definfing methods in controller and implementing in the Service

--- firstapp/src/main/java/com/example/firstapp/Controller/DepartmentController.java
+++ firstapp/src/main/java/com/example/firstapp/Controller/DepartmentController.java
@@ -1,7 +1,32 @@
 package com.example.firstapp.Controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.example.firstapp.Model.Department;
+import com.example.firstapp.Service.DepartmentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class DepartmentController {
+
+    // using the spring @Autowired annotation to get the object which is already created by the spring when we declared the class
+    // so we are not creating the object manually, spring is already providing it (this is called Dependency Injection)
+    @Autowired
+    private DepartmentService departmentService;
+
+    @PostMapping("/departments")
+    public Department saveDepartment(@RequestBody Department department) {
+        return departmentService.saveDepartment(department);
+    }
+
+    @GetMapping("/getDepartments")
+    public List<Department> fetchDepartmentList(){
+        return departmentService.fetchDepartmentList();
+    }
+
+    @GetMapping("/getUserById/{id}")
+    public Department fetchDepartmentById(@PathVariable("id") Long departmentId){
+        return departmentService.fetchDepartmentById(departmentId);
+    }
 }
--- firstapp/src/main/java/com/example/firstapp/Service/DepartmentService.java
+++ firstapp/src/main/java/com/example/firstapp/Service/DepartmentService.java
@@ -1,4 +1,27 @@
 package com.example.firstapp.Service;
 
-public interface DepartmentService {
+import com.example.firstapp.Model.Department;
+import com.example.firstapp.Repository.DepartmentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DepartmentService {
+
+    @Autowired
+    private DepartmentRepository departmentRepository;
+
+    public Department saveDepartment(Department department) {
+        return departmentRepository.save(department);
+    }
+
+    public List<Department> fetchDepartmentList() {
+        return departmentRepository.findAll();
+    }
+
+    public Department fetchDepartmentById(Long departmentId) {
+        return departmentRepository.findById(departmentId).get();
+    }
 }
